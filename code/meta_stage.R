833dbc8fde38d7de3339f98661e53c483bfc18e9
MinxiBen <93164439+MinxiBen@users.noreply.github.com>
2022-06-08 15:36:38 -0400

    Update meta_stage.R

--- code/meta_stage.R
+++ code/meta_stage.R
@@ -4,6 +4,10 @@ library(readxl)
 library(ggplot2)
 library(ggpubr)
 library(dmetar)
+
+#########################################
+#European stage proportion meta analysis#
+#########################################
 Data_Europe<-read_xlsx('C:/learning/R/melanoma/manusript/table/stage_dist_meta.xlsx',1)
 #Data_after_deletion<-Data[c(-1,-3),]
 Europe_meta_stage <- metaprop(III_IV_number, 
@@ -29,7 +33,9 @@ Europe_meta_plot<-forest.meta(Europe_meta_stage,
                               digits=3)
 
 
-
+######################################
+#Asian stage proportion meta analysis#
+######################################
 Data_Asian<-read_xlsx('C:/learning/R/melanoma/manusript/table/stage_dist_meta.xlsx',2)
 #Data_after_deletion<-Data[c(-1,-3),]
 Asian_meta_stage <- metaprop(III_IV_number, 
@@ -54,18 +60,9 @@ Asian_meta_plot<-forest.meta(Asian_meta_stage,
                               fontsize=10, 
                               digits=3)
 
-
-
-
-
-
-
-
-
-
-
-
-
+####################################
+#Egger's regression Publication bias  
+####################################
 Data_comb<-rbind(Data_Europe,Data_Asian)
 Data_comb$Race<-'Europe'
 index<-c(grep('China',Data_comb$Country),
@@ -84,11 +81,12 @@ All_meta_stage <- metaprop(III_IV_number,
 
 metareg(All_meta_stage, ~ Race)
 
-
-
+#############
+#funnel plot
+#############
 funnel.meta(Asian_meta_stage)
 funnel.meta(Europe_meta_stage)
 metabias(All_meta_stage, method.bias = "peters")
-Sys.setlocale(category = "LC_ALL", locale = "us")
-submeta<-subgroup.analysis.mixed.effects(x = All_meta_stage,subgroups = Data_comb$Race)
-forest(submeta$m.random)
+# Sys.setlocale(category = "LC_ALL", locale = "us")
+# submeta<-subgroup.analysis.mixed.effects(x = All_meta_stage,subgroups = Data_comb$Race)
+# forest(submeta$m.random)
